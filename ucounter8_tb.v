0e0dd59f6d891ddd3a1297c59217d8a6aff9865e
Monica Tu <monica.service@gmail.com>
2012-10-03 05:17:45 +0800

    Simple testbench for ucounter8.

--- ucounter8_tb.v
+++ ucounter8_tb.v
@@ -1,14 +1,22 @@
 module ucounter8_tb;
-    reg clk;
-    reg _load, _updown, _areset;
-    reg [7:0]preld_val;
-    ucounter8 g(clk, _areset, _load, _updown, preld_val, dcount);
+
+reg  clk, _areset, _aset, _load, _updown, _wrapstop;
+reg  [7:0] preld_val;
+wire [7:0] dcount;
+wire overflow;
+reg  [7:0] dcount_tb;
+reg  overflow_tb;
+
+    ucounter8 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop);
     initial
     begin
         clk = 0;
-        _areset = 1; _load = 0;
-        _updown = 1; 
-        preld_val=8'b00010111;
+        _areset = 0; 
+        _aset = 0;
+        _load = 1;
+        _updown = 1;
+        _wrapstop = 0;
+        preld_val=8'b11111000;
         $dumpfile("ucounter8_tb");
         $dumpvars;
         #10000 $finish;
@@ -19,13 +27,8 @@ module ucounter8_tb;
         #1 clk = ~clk;
     end
     
-//    always
-//    begin
-//        #3 _load = 1;
-//    end
-    
-    initial
+    always
     begin
-        #2 _areset=0;
+        #3 _load = 0;
     end
 endmodule
