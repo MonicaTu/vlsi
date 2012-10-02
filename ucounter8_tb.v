6f8436c00e5d1def94a2ad1f5d09a6d8cba9a6e2
Monica Tu <monica.service@gmail.com>
2012-10-03 07:18:37 +0800

    (ucounter8_tb) Re-arrange controller.

--- ucounter8_tb.v
+++ ucounter8_tb.v
@@ -30,43 +30,63 @@ integer i;
     begin
         #1 clk = ~clk;
     end
-   
+  
+    always
+    begin
+        #5 _load = 1;
+        #6 _load = 0;
+        #47 _load = 1; 
+        #48 _load = 0;
+    end
+    
+    always
+    begin
+        #21 _updown = 0;
+        #36 _updown = 1;
+    end
+
+    always
+    begin
+        #37 _aset = 1;
+    end
+
+    always
+    begin
+        #42 _areset = 1;
+    end
+
+    always
+    begin
+        #49 _wrapstop = 0;
+    end
+
     always @ (posedge clk) // fixme: posedge vs. clock signal
     begin
         for (i = 0; i < 5; i = i+1) begin
                 #1 dcount_tb = dcount_tb + 1;
         end
         
-        #1 _load = 1; 
         #1 dcount_tb = 8'b11111000;
-        #1 _load = 0; 
        
         for (i = 0; i < 15; i = i + 1) begin
             #1 dcount_tb = dcount_tb + 1;
         end
 
-        #1 _updown = 0;
         for (i = 0; i < 15; i = i + 1) begin
             #1 dcount_tb = dcount_tb - 1;
         end
         
-        #1 _aset = 1; 
-        #1 _updown = 1;
         for (i = 0; i < 5; i = i + 1) begin
             #1 dcount_tb = dcount_tb;
         end
 
-        #1 _areset = 1; 
         #1 dcount_tb = 0;
         
         for (i = 0; i < 5; i = i + 1) begin
             #1 dcount_tb = dcount_tb + 1;
         end
 
-        #1 _load = 1; 
         #1 dcount_tb = 8'b11111000;
-        #1 _load = 0;
-        #1 _wrapstop = 0;
         for (i = 0; i < 15; i = i + 1) begin
             #1 dcount_tb = dcount_tb + 1;
             if (dcount_tb == 8'b11111111) begin
