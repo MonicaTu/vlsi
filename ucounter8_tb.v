bf9e01046de62fca08cd8ffa56c81f9ddd1da258
Monica Tu <monica.service@gmail.com>
2012-10-07 06:39:47 +0800

    (ucounter_8) To-do: _load signal.

--- ucounter8_tb.v
+++ ucounter8_tb.v
@@ -2,7 +2,8 @@
 `timescale 1ns/1ns
 
 module ucounter8_tb;
-
+parameter CLK_CYCLE = 2;
+parameter DEFAULT_VAL = 8'b11111000;
 reg  clk, _areset, _aset, _load, _updown, _wrapstop;
 reg  [7:0] preld_val;
 wire [7:0] dcount;
@@ -20,32 +21,36 @@ integer i;
         _load = 0;
         _updown = 1;
         _wrapstop = 0;
-        preld_val = 8'b11111000;
-
-        dcount_tb = 0;
-        overflow_tb = 0;
+        preld_val = DEFAULT_VAL;
+        
         $dumpfile("ucounter8_tb");
         $dumpvars;
         #10000 $finish;
     end
     
     always begin
-        #1 clk = ~clk;
+        #(CLK_CYCLE/2) clk = ~clk;
+    end
+    
+    always @ (posedge clk) begin
+        if (!_areset && !_load) begin
+            if (_updown)
+                dcount_tb = dcount_tb + 1;
+            else
+                dcount_tb = dcount_tb - 1;
+        end
+    end
+    
+    initial begin
+        #CLK_CYCLE _areset = 0;
+        #(CLK_CYCLE*5) _load = 1;
+        #CLK_CYCLE _load = 0;
     end
  
     initial begin
-        _areset = 1;
-        #3 _areset = 0;
-        #5 _load = 1;
+        #1 dcount_tb = 0;
+        #(CLK_CYCLE)dcount_tb = 0;
+        #(CLK_CYCLE*5+1) dcount_tb = DEFAULT_VAL;
     end
 
-    initial begin
-        dcount_tb = 0;
-        #3;
-        for (i = 0; i < 5; i = i+1) begin
-            dcount_tb = dcount_tb + 1;
-            #1;
-        end
-        dcount_tb = 8'b11111000;
-    end
 endmodule
