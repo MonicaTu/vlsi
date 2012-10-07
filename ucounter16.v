e53e5bc817fa7ec8f596eb9ba2a6959b24d03de0
Monica Tu <monica.service@gmail.com>
2012-10-07 14:18:42 +0800

    (ucounter16) Remove redundance.

--- ucounter16.v
+++ ucounter16.v
@@ -5,18 +5,9 @@ input clk, _areset, _aset, _load, _updown, _wrapstop;
 input  [15:0] preld_val_top;
 output [15:0] dcount_top;
 output overflow;
-
-input  clk_low;
 output overflow_low;
 
-input  clk_high;
-output overflow_high;
-
     ucounter8 ucnt0(overflow_low, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop);
-    ucounter8 ucnt1(overflow_high, dcount_top[15:8], overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
-
-    always @ (posedge clk, _areset, _aset, _load, _updown, _wrapstop) begin
-    
-    end
+    ucounter8 ucnt1(overflow, dcount_top[15:8], overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
 
 endmodule
