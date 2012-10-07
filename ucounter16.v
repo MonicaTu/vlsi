be59b06dc26b030f0b74a89aaf5825bf8e1bbbbb
Monica Tu <monica.service@gmail.com>
2012-10-07 13:13:48 +0800

    (ucounter16) To-do: dcounter(16 bit). Low bits counter looks good.

--- ucounter16.v
+++ ucounter16.v
@@ -1,39 +1,31 @@
 `include "ucounter8.v"
 
-module ucounter16 (overflow_high, overflow_low, dcount_high, dcount_low, clk_high, clk_low, _areset_high, _areset_low, _aset_high, _aset_low, _load_high, _load_low, preld_val_high, preld_val_low, _updown, _wrapstop);
-input _updown, _wrapstop;
-output  clk_low;
-reg  clk_low;
-input _areset_low, _aset_low, _load_low;
-input  [7:0] preld_val_low;
-output [7:0] dcount_low;
+module ucounter16 (overflow, dcount_15, clk, _areset, _aset, _load, preld_val_15, _updown, _wrapstop);
+input clk, _areset, _aset, _load, _updown, _wrapstop;
+input  [15:0] preld_val_15;
+output [15:0] dcount_15;
+output overflow;
+reg    [15:0] dcount_15;
+reg    overflow;
+
+input  clk_low;
+output [7:0]dcount_low;
 output overflow_low;
 
-output  clk_high;
-reg    clk_high; 
-input  _areset_high, _aset_high, _load_high;
-input  [7:0] preld_val_high;
-output [7:0] dcount_high;
+input  clk_high;
+output [7:0]dcount_high;
 output overflow_high;
 
-    ucounter8 ucnt0(overflow_low, dcount_low, clk_low, _areset_low, _aset_low, _load_low, preld_val_low, _updown, _wrapstop);
-    ucounter8 ucnt1(overflow_high, dcount_high, clk_high, _areset_high, _aset_high, _load_high, preld_val_high, _updown, _wrapstop);
-   
+    ucounter8 ucnt0(overflow_low, dcount_low, clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop);
+    ucounter8 ucnt1(overflow_high, dcount_high, overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
+
     initial begin
-        clk_low = 0;
-        clk_high = 0;
+        
     end
 
-    always begin
-        #1 clk_low = ~clk_low;
-    end
-
-    always @ (posedge clk_low) begin
+    always @ (posedge clk) begin
         if (overflow_high)
             $finish;
-
-        if (overflow_low)
-            #1 clk_high = ~clk_high;
     end
 
 endmodule
--- ucounter16_tb.v
+++ ucounter16_tb.v
@@ -3,70 +3,38 @@
 
 module ucounter16_tb;
 parameter CLK_CYCLE = 2;
-parameter DEFAULT_VAL = 8'b11111000;
-wire  clk_low, clk_high;
-reg  _updown, _wrapstop;
-reg  _areset_low, _aset_low, _load_low;
-reg  _areset_high, _aset_high, _load_high;
-
-input [7:0] preld_val_low;
-input [7:0] preld_val_high;
-
-wire  [7:0] dcount_low;
-wire  overflow_low;
-reg   [7:0] dcount_low_tb;
-reg   overflow_low_tb;
-
-wire  [7:0] dcount_high;
-wire  overflow_high;
-reg   [7:0] dcount_high_tb;
-reg   overflow_high_tb;
+parameter DEFAULT_VAL = 16'b0000_0000_11111_000;
+reg  clk, _areset, _aset, _load, _updown, _wrapstop;
+reg  [15:0] preld_val;
+wire [15:0] dcount;
+wire overflow;
+reg  [15:0] dcount_tb;
+reg  overflow_tb;
 integer i;
 
-//    ucounter16 m(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop);
-//    ucounter16 m(overflow_high, overflow_low, dcount_high, dcount_low, clk, _areset, _aset, _load, preld_val_high, preld_val_low, _updown, _wrapstop);
-    ucounter16 m(overflow_high, overflow_low, dcount_high, dcount_low, clk_high, clk_low, _areset_high, _areset_low, _aset_high, _aset_low, _load_high, _load_low, preld_val_high, preld_val_low, _updown, _wrapstop);
+    ucounter16 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop);
     initial
     begin
-//        clk_low = 0;
-        _areset_low = 1; 
-        _aset_low = 0;
-        _load_low = 0;
+        clk = 0;
+        _areset = 1; 
+        _aset = 0;
+        _load = 0;
         _updown = 1;
         _wrapstop = 1;
-//        preld_val = DEFAULT_VAL;
-        
-//        clk_high = 0;
-        _areset_high = 1; 
-        _aset_high = 0;
-        _load_high = 0;
+        preld_val = DEFAULT_VAL;
         
         $dumpfile("ucounter16_tb");
         $dumpvars;
         #10000 $finish;
     end
     
-//    always begin
-//        #(CLK_CYCLE/2) clk_low = ~clk_low;
-//    end
+    always begin
+        #(CLK_CYCLE/2) clk = ~clk;
+    end
     
     initial begin
-        #CLK_CYCLE _areset_low = 0;
-        _areset_high = 0;
-//        #(CLK_CYCLE*5) _load_low = 1;
-//        #CLK_CYCLE _load_low = 0;
+        #CLK_CYCLE _areset = 0;
+//        #(CLK_CYCLE*5) _load = 1;
+//        #CLK_CYCLE _load = 0;
     end
- 
-    initial begin
-        #1 dcount_low_tb = 0;
-        for (i = 0; i < 5; i = i + 1) begin
-            #(CLK_CYCLE) dcount_low_tb = dcount_low_tb + 1;
-        end
-
-        #(CLK_CYCLE) dcount_low_tb = DEFAULT_VAL;
-        for (i = 0; i < 10; i = i + 1) begin
-            #(CLK_CYCLE) dcount_low_tb = dcount_low_tb + 1;
-        end
-    end
-
 endmodule
