14804f6fba5068dea72a2102ffc5783aa17a8223
Monica Tu <monica.service@gmail.com>
2012-10-07 13:50:35 +0800

    (ucounter16) Remove all registers.

--- ucounter16.v
+++ ucounter16.v
@@ -1,31 +1,22 @@
 `include "ucounter8.v"
 
-module ucounter16 (overflow, dcount_15, clk, _areset, _aset, _load, preld_val_15, _updown, _wrapstop);
+module ucounter16 (overflow, dcount_top, clk, _areset, _aset, _load, preld_val_top, _updown, _wrapstop);
 input clk, _areset, _aset, _load, _updown, _wrapstop;
-input  [15:0] preld_val_15;
-output [15:0] dcount_15;
+input  [15:0] preld_val_top;
+output [15:0] dcount_top;
 output overflow;
-reg    [15:0] dcount_15;
-reg    overflow;
 
 input  clk_low;
-output [7:0]dcount_low;
 output overflow_low;
 
 input  clk_high;
-output [7:0]dcount_high;
 output overflow_high;
 
-    ucounter8 ucnt0(overflow_low, dcount_low, clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop);
-    ucounter8 ucnt1(overflow_high, dcount_high, overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
+    ucounter8 ucnt0(overflow_low, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop);
+    ucounter8 ucnt1(overflow_high, dcount_top[15:8], overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
 
-    initial begin
-        
-    end
-
-    always @ (posedge clk) begin
-        if (overflow_high)
-            $finish;
+    always @ (posedge clk, _areset, _aset, _load, _updown, _wrapstop) begin
+    
     end
 
 endmodule
