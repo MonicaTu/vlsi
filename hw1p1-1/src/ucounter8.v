7f85f631793f70de2c2fe13ac11f8ba4db365200
Monica Tu <monica.service@gmail.com>
2012-10-09 19:25:15 +0800

    (ucounter8) Add new signal, carry-in.

--- hw1p1-1/src/ucounter8.v
+++ hw1p1-1/src/ucounter8.v
@@ -1,34 +1,51 @@
 /* module: universal 8-bit  counter/timer */
 
-module ucounter8 (overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop);
+module ucounter8 (overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop, carry_in);
+
 parameter MAX8BIT_VAL = 8'b11111111;
 parameter MIN8BIT_VAL = 8'b00000000;
 parameter RESET_VAL = 8'b00000000;
-input  clk, _areset, _aset, _load, _updown, _wrapstop;
+
+input  clk, _areset, _aset, _load, _updown, _wrapstop, carry_in;
 input  [7:0] preld_val;
+
 output [7:0] dcount;
 output overflow;
+
 reg    [7:0] dcount;
 reg    overflow;
 
-    // TODO: overflow --> clk when _wrapstop is 0.
+wire   carry_out;
+wire   local_clk;
 
-    always @ (posedge clk) begin
+assign carry_out = ( dcount == MAX8BIT_VAL ) ? 1 : 0;
+assign local_clk = _wrapstop ? 1 : clk;
+
+    always @ (posedge local_clk) begin
+        if (_areset)
+    	    overflow <= 0;
+    	else begin
+            if (carry_out == 1)
+    		   overflow = 1;
+    		else
+    		   overflow = 0;
+    	end
+    end
+
+    always @ (posedge local_clk) begin
         if (_areset)
     	    dcount <= RESET_VAL;
     	else if (_aset)
     	    dcount <= MAX8BIT_VAL;
     	else if (_load)
     	    dcount <= preld_val;
-    	else begin
-            if (_updown) begin
-                  dcount <= ( dcount == MAX8BIT_VAL) ? ( ( _wrapstop == 1 ) ? RESET_VAL : dcount ) : dcount + 8'd1;
-                  overflow <= ( dcount == MAX8BIT_VAL && _wrapstop == 0 ) ? 1 : 0;
-            end else begin
-                  dcount <= ( dcount == MIN8BIT_VAL) ? ( ( _wrapstop == 1 ) ? dcount : RESET_VAL ) : dcount - 8'd1;
-                  overflow <= ( dcount == RESET_VAL && _wrapstop == 0 ) ? 1 : 0;
-            end
-    	end
+        
+        if (carry_in) begin
+            if (_updown)
+                dcount = dcount + 8'd1;
+            else
+                dcount = dcount - 8'd1;
+        end
     end
 
 endmodule
