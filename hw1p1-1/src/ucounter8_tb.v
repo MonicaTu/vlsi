6e2c134f36a2548cf6d039d619c4fc9a7ce18ceb
Monica Tu <monica.service@gmail.com>
2012-10-09 16:39:10 +0800

    (ucounter8_tb) Handle overflow_tb.

--- hw1p1-1/src/ucounter8_tb.v
+++ hw1p1-1/src/ucounter8_tb.v
@@ -1,9 +1,16 @@
 //`timescale 1ns/10ps
 `timescale 1ns/1ns
 
+`include "ucounter8.v"
+
 module ucounter8_tb;
 parameter CLK_CYCLE = 2;
 parameter DEFAULT_VAL = 8'b11111000;
+parameter MAX8BIT_VAL = 8'b11111111;
+parameter MIN8BIT_VAL = 8'b00000000;
+parameter RESET_VAL = 8'b00000000;
+parameter ON = 1'b1;
+parameter OFF = 1'b0;
 reg  clk, _areset, _aset, _load, _updown, _wrapstop;
 reg  [7:0] preld_val;
 wire [7:0] dcount;
@@ -20,7 +27,7 @@ integer i;
         _aset = 0;
         _load = 0;
         _updown = 1;
-        _wrapstop = 1;
+        _wrapstop = 0;
         preld_val = DEFAULT_VAL;
         
         $dumpfile("ucounter8_tb");
@@ -39,14 +46,18 @@ integer i;
     end
  
     initial begin
-        #1 dcount_tb = 0;
+        #1 dcount_tb = RESET_VAL;
         for (i = 0; i < 5; i = i + 1) begin
             #(CLK_CYCLE) dcount_tb = dcount_tb + 1;
         end
 
         #(CLK_CYCLE) dcount_tb = DEFAULT_VAL;
         for (i = 0; i < 10; i = i + 1) begin
-            #(CLK_CYCLE) dcount_tb = dcount_tb + 1;
+            // _wrapstop = 0;
+            if (dcount_tb == MAX8BIT_VAL) begin
+                overflow_tb = 1;
+            end else
+                #(CLK_CYCLE) dcount_tb = dcount_tb + 1;
         end
     end
 
