85982afadf8843c50746874c83c7d555f304013c
Monica Tu <monica.service@gmail.com>
2012-10-10 01:54:13 +0800

    (ucounter8_tb) Copy from ucounter16_tb. SIZE = 8.

--- hw1p1-1/src/ucounter8_tb.v
+++ hw1p1-1/src/ucounter8_tb.v
@@ -1,76 +1,101 @@
-//`timescale 1ns/10ps
-`timescale 1ns/1ns
-
+`timescale 1ns/10ps
 `include "ucounter8.v"
+
+`define PERIOD 10
+`define SIZE 8
 
 module ucounter8_tb;
 
-parameter CLK_CYCLE = 2;
-parameter DEFAULT_VAL = 8'b11111000;
-parameter MAX8BIT_VAL = 8'b11111111;
-parameter MIN8BIT_VAL = 8'b00000000;
-parameter RESET_VAL = 8'b00000000;
-parameter ON = 1'b1;
-parameter OFF = 1'b0;
-
-reg  clk, _areset, _aset, _load, _updown, _wrapstop, carry_in;
-reg  [7:0] preld_val;
-
-wire [7:0] dcount;
-wire overflow;
-
-reg  [7:0] tb_dcount;
-reg  tb_overflow;
-
-wire carry_out; 
-integer i;
+    parameter DEFAULT_VAL = 8'hC5;
+    parameter MAX8BIT_VAL = 8'hFF;
     
-    ucounter8 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop, carry_in);
-
-    assign carry_out = ( dcount == MAX8BIT_VAL && _wrapstop ) ? 1 : 0;
+    reg clk, _areset, _aset, _load, _updown, _wrapstop;
+    reg  [`SIZE-1:0] preld_val;
+    reg  _carry_in;
     
+    wire [`SIZE-1:0] dcount;
+    wire overflow;
+    
+    reg [`SIZE-1:0] tb_dcount;
+    reg tb_overflow;
+    integer i;
+
+    ucounter8 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop, _carry_in);
+
+	// setup clk
     initial begin
-        clk = 0;
-        _areset = 1; 
-        _aset = 0;
-        _load = 0;
-        _updown = 1;
-        _wrapstop = 1;
-        carry_in = 1;
+        clk = 0; // Start clock from LOW
+        forever #(`PERIOD/2) clk = ~clk;
+    end   
+
+    initial begin
         preld_val = DEFAULT_VAL;
-        #10000 $finish;
+        _updown = 1;
+        _carry_in = 1;
+        #100000 $finish;
     end
-    
-    always begin
-        #(CLK_CYCLE/2) clk = ~clk;
-    end
-    
-    initial begin
-        #CLK_CYCLE _areset = 0;
-        #(CLK_CYCLE*5) _load = 1;
-        #CLK_CYCLE _load = 0;
-    end
- 
-    initial begin
-        #1 tb_dcount = RESET_VAL;
-        for (i = 0; i < 5; i = i + 1) begin
-            #(CLK_CYCLE) tb_dcount = tb_dcount + 1;
-        end
 
-        #(CLK_CYCLE) tb_dcount = DEFAULT_VAL;
-        for ( i = 0; i < 10; i = i + 1 ) begin
-            // _wrapstop = 1;
-            if ( carry_out == 1 ) begin
-                tb_dcount = 0;
-                tb_overflow = 1;
-            end else
-                #(CLK_CYCLE) tb_dcount = tb_dcount + 1;
+	initial begin
+        // _areset test
+	                 _areset   = 0; 
+        #(`PERIOD)   _areset   = 1;
+	    #(`PERIOD)   _areset   = 0;
+        // _load test
+        #(`PERIOD*5) _load     = 1;
+        #(`PERIOD)   _load     = 0;
+        // _updown test 
+        #(`PERIOD*5) _updown   = 0;
+        #(`PERIOD*5) _updown   = 1;
+        // _wrapstop test
+                     _wrapstop = 1;
+        // _aset test
+                     _aset     = 1;
+        #(`PERIOD)   _aset     = 0;
+        // _wrapstop test
+        #(`PERIOD*5) _wrapstop = 0;
+        // _aset test
+                     _aset     = 1;
+        #(`PERIOD)   _aset     = 0;
+        #(`PERIOD*5);
+	end
+
+    initial begin
+        // _areset test
+        #(`PERIOD*1.5) tb_dcount = 0;
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
         end
+        // _load test
+        #(`PERIOD) tb_dcount = DEFAULT_VAL; 
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
+        end
+        // _updown test
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount - 1;
+        end
+        // _aset test
+        #(`PERIOD) tb_dcount = MAX8BIT_VAL; 
+        // _wrapstop test
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
+        end
+        // _aset test
+        #(`PERIOD) tb_dcount = MAX8BIT_VAL; 
+        // _wrapstop test
+        #(`PERIOD*5);
+        tb_overflow = 1;
+        $finish;
     end
     
-    initial
-    begin
+    initial begin
         $dumpfile("ucounter8_tb.vcd");
-        $dumpvars;
+        $dumpvars();
     end
+
+//    initial begin
+//        $fsdbDumpfile("ucounter8_tb.fsdb");
+//        $fsdbDumpvars();
+//    end
+
 endmodule
