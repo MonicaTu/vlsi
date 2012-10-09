db64419556e49b27425e9e98caeadbfe90dccaf5
Monica Tu <monica.service@gmail.com>
2012-10-09 23:22:40 +0800

    (Base)(ucounter16) Handle wrapstop for low & high.

--- hw1p1-1/src/ucounter16.v
+++ hw1p1-1/src/ucounter16.v
@@ -10,7 +10,13 @@ output overflow;
 
 wire low_overflow;
 
-    ucounter8 high(overflow, dcount_top[15:8], clk, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop, low_overflow);
-    ucounter8 low(low_overflow, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop, carry_in);
+wire low_wrapstop;
+wire high_wrapstop;
+
+assign low_wrapstop  = _wrapstop;
+assign high_wrapstop = 1;
+
+    ucounter8 high(overflow, dcount_top[15:8], clk, _areset, _aset, _load, preld_val[15:8], _updown, high_wrapstop, low_overflow);
+    ucounter8 low(low_overflow, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, low_wrapstop, carry_in);
 
 endmodule
