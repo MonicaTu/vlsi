ad0d5afbe16385cc97d820883df3b4896bc3e0f0
Monica Tu <monica.service@gmail.com>
2012-10-10 01:32:15 +0800

    (Base)(ucounter16_tb) Test all signals.

--- hw1p1-1/src/ucounter16_tb.v
+++ hw1p1-1/src/ucounter16_tb.v
@@ -1,16 +1,15 @@
-//`timescale 1ns/10ps
-`timescale 1ns/1ns
+`timescale 1ns/10ps
 `include "ucounter16.v"
 
+`define PERIOD 10
 module ucounter16_tb;
 
-//parameter DEFAULT_VAL = 8'b10011010;
-parameter DEFAULT_VAL = 8'hF8;
+parameter DEFAULT_VAL = 16'hABC5;
 parameter MAX16BIT_VAL = 16'hFFFF;
 
 reg clk, _areset, _aset, _load, _updown, _wrapstop;
 reg  [15:0] preld_val;
-reg  carry_in;
+reg  _carry_in;
 
 wire [15:0] dcount;
 wire overflow;
@@ -19,111 +18,76 @@ reg [15:0] tb_dcount;
 reg tb_overflow;
 integer i;
 
-    ucounter16 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop, carry_in);
+    ucounter16 g(overflow, dcount, clk, _areset, _aset, _load, preld_val, _updown, _wrapstop, _carry_in);
+
+	// setup clk
+    initial begin                                                                                      
+        clk = 0; // Start clock from LOW                                                              
+        forever #(`PERIOD/2) clk = ~clk;                                                            
+    end   
 
     initial begin
-        clk = 0;
-	    _areset = 0; 
-	    _aset = 0;
-        _load = 0;
+//        _aset = 0;
+//        _load = 0;
         preld_val = DEFAULT_VAL;
         _updown = 1;
-        _wrapstop = 1;
-        carry_in = 1;
+//        _wrapstop = 1;
+        _carry_in = 1;
         #100000 $finish;
     end
 
-	//setup clk
-    always begin
-        #1 clk = ~clk;
-    end
-
-    // _areset test
 	initial begin // 0-50
-        #2 _areset = 1; //20
-	    #2 _areset = 0; //40
+        // _areset test
+	                 _areset   = 0; 
+        #(`PERIOD)   _areset   = 1;
+	    #(`PERIOD)   _areset   = 0;
+        // _load test
+        #(`PERIOD*5) _load     = 1;
+        #(`PERIOD)   _load     = 0;
+        // _updown test 
+        #(`PERIOD*5) _updown   = 0;
+        #(`PERIOD*5) _updown   = 1;
+        // _wrapstop test
+                     _wrapstop = 1;
+        // _aset test
+                     _aset     = 1;
+        #(`PERIOD)   _aset     = 0;
+        // _wrapstop test
+        #(`PERIOD*5) _wrapstop = 0;
+        // _aset test
+                     _aset     = 1;
+        #(`PERIOD)   _aset     = 0;
+        #(`PERIOD*5);
 	end
 
-//    initial begin
-//        #2 tb_dcount = 0;
-//    end
-/*
-    // _aset test
-    initial begin //50-100
-	    	_aset = 0;
-        #50 _aset = 1; //50
-	    #10	_aset = 0; //60
-	end	
-
     initial begin
+        // _areset test
+        #(`PERIOD*1.5) tb_dcount = 0;
         for ( i = 0; i < 5; i = i + 1 ) begin
-            #10 tb_dcount = tb_dcount + 1;
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
         end
-        #10 tb_dcount = MAX16BIT_VAL;
-    end
-*/
-    // _laod test
-    initial begin //100-200
-        #5 _load = 1; //100
-        #2 _load = 0; //190
-    end
-
-//    initial begin
-//        #10 tb_dcount = DEFAULT_VAL; 
-//    end
-/*
-    // _updown test
-	initial begin //200-1000
-        	  _updown = 1;
-	    #200  _updown = 0;
-	    #1000 _updown = 1;
-	end	
-
-    initial begin
-        for ( i = 0; i < 80; i = i + 1 ) begin
-            #10 tb_dcount = tb_dcount + 1;
+        // _load test
+        #(`PERIOD) tb_dcount = DEFAULT_VAL; 
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
         end
-    end
-
-    // _wrapstop test
-    initial begin //1200 Before circle, After fullStop
-		  _wrapstop = 0;
-	#1000 _wrapstop = 1;
-	end
-
-    initial begin
-        for ( i = 0; i < 100; i = i + 1 ) begin
-            #10 tb_dcount = tb_dcount + 1;
-            if (tb_dcount == MAX16BIT_VAL ) begin
-                tb_dcount = tb_dcount;
-                tb_overflow = 1;
-            end
+        // _updown test
+//        #(`PERIOD) tb_dcount = DEFAULT_VAL; 
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount - 1;
         end
-    end
-
-    initial begin //total 1500
-    #5000 $finish;
-    end
-
-    initial begin
-        for ( i = 0; i < 500; i = i + 1 ) begin
-            #10 tb_dcount = tb_dcount + 1;
-            if (tb_dcount == MAX16BIT_VAL ) begin
-                tb_dcount = 0;
-                $finish;
-            end
+        // _aset test
+        #(`PERIOD) tb_dcount = MAX16BIT_VAL; 
+        // _wrapstop test
+        for ( i = 0; i < 5; i = i + 1 ) begin
+            #(`PERIOD) tb_dcount = tb_dcount + 1;
         end
+        // _aset test
+        #(`PERIOD) tb_dcount = MAX16BIT_VAL; 
+        // _wrapstop test
+        #(`PERIOD*5);
     end
-*/
-/* 
-    initial begin //1200 Before circle, After fullStop
-			_wrapstop = 0;
-	end	
     
-    initial begin //total 1500
-    #50000 $finish;
-    end
- */    
     initial begin
         $dumpfile("ucounter16_tb.vcd");
         $dumpvars();
