fa6c9a3cc1d0c05280f82e64849cfa1156af1dac
Monica Tu <monica.service@gmail.com>
2012-10-09 19:24:18 +0800

    (ucounter16) Add carry-in signal as input.

--- hw1p1-1/src/ucounter16.v
+++ hw1p1-1/src/ucounter16.v
@@ -1,13 +1,16 @@
 `include "ucounter8.v"
 
-module ucounter16 (overflow, dcount_top, clk, _areset, _aset, _load, preld_val_top, _updown, _wrapstop);
-input clk, _areset, _aset, _load, _updown, _wrapstop;
+module ucounter16 (overflow, dcount_top, clk, _areset, _aset, _load, preld_val_top, _updown, _wrapstop, carry_in);
+
+input clk, _areset, _aset, _load, _updown, _wrapstop, carry_in;
 input  [15:0] preld_val_top;
+
 output [15:0] dcount_top;
 output overflow;
-output overflow_low;
 
-    ucounter8 ucnt0(overflow_low, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop);
-    ucounter8 ucnt1(overflow, dcount_top[15:8], overflow_low, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop);
+wire low_overflow;
+
+    ucounter8 high(overflow, dcount_top[15:8], clk, _areset, _aset, _load, preld_val[15:8], _updown, _wrapstop, low_overflow);
+    ucounter8 low(low_overflow, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, _wrapstop, carry_in);
 
 endmodule
