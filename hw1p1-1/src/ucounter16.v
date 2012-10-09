9a730f1be4784746d30ef454d79ea084c31cb0ce
Monica Tu <monica.service@gmail.com>
2012-10-10 04:20:05 +0800

    (ucounter16) Add high_overflow.

--- hw1p1-1/src/ucounter16.v
+++ hw1p1-1/src/ucounter16.v
@@ -9,6 +9,7 @@ output [15:0] dcount_top;
 output overflow;
 
 wire low_overflow;
+wire high_overflow;
 
 wire low_wrapstop;
 wire high_wrapstop;
@@ -16,7 +17,9 @@ wire high_wrapstop;
 assign low_wrapstop  = _wrapstop;
 assign high_wrapstop = 1;
 
-    ucounter8 high(overflow, dcount_top[15:8], clk, _areset, _aset, _load, preld_val[15:8], _updown, high_wrapstop, low_overflow);
+assign overflow = _wrapstop ? 0 : high_overflow;
+
+    ucounter8 high(high_overflow, dcount_top[15:8], clk, _areset, _aset, _load, preld_val[15:8], _updown, high_wrapstop, low_overflow);
     ucounter8 low(low_overflow, dcount_top[7:0], clk, _areset, _aset, _load, preld_val[7:0], _updown, low_wrapstop, carry_in);
 
 endmodule
