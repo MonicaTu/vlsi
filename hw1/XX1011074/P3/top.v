a6b4f1e5fa3c73c9cb85cda29b0a667563ab21e6
Monica Tu <monica.service@gmail.com>
2012-10-20 22:48:49 +0800

    (hw1P3) Re-arrange the connections between reg_file and alu32.

--- hw1/XX1011074/P3/top.v
+++ hw1/XX1011074/P3/top.v
@@ -5,54 +5,51 @@ module top(alu_result, alu_overflow, clk, rst, read_address1, read_address2, wri
   
   parameter DataSize = 32;
   parameter AddrSize = 5;
-  // top
+ 
+  /* OUT */
+  output alu_overflow;
+  output [DataSize-1:0]alu_result;
+
+  /* IN */
+  input clk;
   input rst;
+  //Register
+  input [AddrSize-1:0]read_address1;
+  input [AddrSize-1:0]read_address2;
+  input [AddrSize-1:0]write_address;
+  input enable_fetch;
+  input enable_writeback;
+  //imm_sel
   input [4:0]imm_5bit;
   input [14:0]imm_15bit;
   input [19:0]imm_20bit;
   input [1:0]mux4to1_select;
-  input mux2to1_select, imm_reg_select;
-
-  // alu
-  output [DataSize-1:0]alu_result;
-  output alu_overflow;
-  
-//  wire [DataSize-1:0]scr1,scr2;
-  input [5:0]opcode;
-  input [4:0]sub_opcode;
+  input mux2to1_select;
+  input imm_reg_select;
+  //ALU
   input enable_execute;
-  
-//  reg [63:0]rotate;
-//  reg a,b;
+  input [5:0] opcode;
+  input [4:0] sub_opcode;
 
-  //regfile
-  input clk;
-  output [DataSize-1:0]read_data1;
-  output [DataSize-1:0]read_data2;
+  /* Wire */
+  wire [DataSize-1:0]read_data1;
+  wire [DataSize-1:0]read_data2;
+//  wire reset;
 
-  input [AddrSize-1:0]read_address1;
-  input [AddrSize-1:0]read_address2;
-  input [AddrSize-1:0]write_address;
-  input [DataSize-1:0]write_data;
-  input enable_fetch, enable_writeback; 
-//  input reset;
-
-  reg [DataSize-1:0]rw_reg[31:0];
-
-  regfile rf (
+  regfile regfile1 (
     .read_data1(read_data1), 
     .read_data2(read_data2),
     .read_address1(read_address1),
     .read_address2(read_address2),
     .write_address(write_address),
-    .write_data(write_data),
+    .write_data(alu_result),
     .clk(clk),
-//    .reset(rst),
-    .reset(reset),
+    .reset(rst),
+//    .reset(reset),
     .read(enable_fetch),
     .write(enable_writeback));
 
-  alu32 alu ( 
+  alu32 alu1 ( 
     .alu_result(alu_result),
     .alu_overflow(alu_overflow),
     .scr1(read_data1),
@@ -60,6 +57,6 @@ module top(alu_result, alu_overflow, clk, rst, read_address1, read_address2, wri
     .opcode(opcode),
     .sub_opcode(sub_opcode),
     .enable_execute(enable_execute),
-//    .reset(rst));
-    .reset(reset));
+    .reset(rst));
+//    .reset(reset));
 endmodule
