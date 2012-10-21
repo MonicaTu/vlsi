0e4421a6dbe93bd73e39dfd2c6ff6a5e8a9faa03
Monica Tu <monica.service@gmail.com>
2012-10-22 01:28:14 +0800

    (hw1P4) Handle mux4to1_select, imm_reg_select, writeback_select.

--- hw1/XX1011074/P4/top.v
+++ hw1/XX1011074/P4/top.v
@@ -6,31 +6,60 @@ module top (instruction, clk, reset);
   parameter DataSize = 32;
   parameter AddrSize = 5;
 
-  /* IN */
+  // top
   input [DataSize-1:0]instruction;
   input clk;
   input reset;
-  input rst; // FIXME
-
-  // instruction
-//  input [4:0] imm_5bit  = instruction[15:10];
-//  input [14:0]imm_15bit = instruction[14:0];
-//  input [19:0]imm_20bit = instruction[19:0];
-
   
   // controller
   wire enable_execute;
   wire enable_fetch;
   wire enable_writeback;
-  wire [5:0]opcode; // FIXME: = instruction[30:25];
-  wire [4:0]sub_opcode; // FIXME: = instruction[4:0];
+  wire [5:0]opcode;
+  wire [4:0]sub_opcode;
   wire mux4to1_select;
   wire writeback_select;
   wire imm_reg_select;
-//  input clock;
-//  input reset;
   input [31:0] PC; // TODO: what is this?
-//  input [31:0] ir; // instruction
+  
+  // regfile
+  wire [DataSize-1:0]read_data1;
+  wire [DataSize-1:0]read_data2;
+
+  wire [AddrSize-1:0]read_address1;
+  wire [AddrSize-1:0]read_address2;
+  wire [AddrSize-1:0]write_address;
+  wire [DataSize-1:0]write_data;
+  
+  // alu
+  wire [DataSize-1:0]scr2;
+  wire [DataSize-1:0]alu_result;
+  output alu_overflow;
+  
+  // others
+  reg  [DataSize-1:0]mux4to1_out;
+  wire [4:0]imm_5bit;
+  wire [14:0]imm_15bit;
+  wire [19:0]imm_20bit;
+
+  initial begin
+    case (mux4to1_select)
+      2'b00: begin
+        // TODO: 5bit ZE; mux4to1_out = ZE(imm_5bit)
+      end
+      2'b01: begin
+        // TODO: 15bit SE: mux4to1_out = SE(imm_15bit)
+      end
+      2'b10: begin
+        // TODO: 15bit ZE; mux4to1_out = ZE(imm_15bit)
+      end
+      2'b11: begin
+        // TODO: 20bit SE; mux4to1_out = SE(imm_20bit)
+      end
+    endcase
+  end
+  assign scr2 = (imm_reg_select) ? mux4to1_select_out: read_data2;
+  assign write_data = (writeback_select) ? alu_result : scr2;
 
   controller conrtoller1 (
     .enable_execute(enable_execute),
@@ -42,24 +71,10 @@ module top (instruction, clk, reset);
     .writeback_select(writeback_select),
     .imm_reg_select(imm_reg_select),
     .clock(clk),
-    .reset(rst),
-//    .reset(reset),
+    .reset(reset),
     .PC(PC),
     .ir(instruction));
 
-  //regfile
-  wire [DataSize-1:0]read_data1;
-  wire [DataSize-1:0]read_data2;
-
-  input [AddrSize-1:0]read_address1;// FIXME: = instruction[19:15];
-  input [AddrSize-1:0]read_address2;// FIXME: = instruction[14:10];
-  input [AddrSize-1:0]write_address;// FIXME: = instruction[24:20];
-  input [DataSize-1:0]write_data   ;// FIXME: = instruction[19:0];  // TODO: Rt32bit=SE(imm20bit)
-//  input clk;
-//  input reset;
-//  input read;
-//  input write;
-  
   regfile regfile1 (
     .read_data1(read_data1), 
     .read_data2(read_data2),
@@ -68,30 +83,18 @@ module top (instruction, clk, reset);
     .write_address(write_address),
     .write_data(write_data),
     .clk(clk),
-    .reset(rst),
-//    .reset(reset),
+    .reset(reset),
     .read(enable_fetch),
     .write(enable_writeback));
-
-  // alu
-  wire [DataSize-1:0]alu_result;
-  wire alu_overflow;
-
-//  input [DataSize-1:0]scr1,scr2;
-//  input [5:0]opcode; // FIXME: = instruction[30:25];
-//  input [4:0]sub_opcode; // FIXME: = instruction[4:0];
-//  input enable_execute;
-//  input reset;
 
   alu32 alu1 ( 
     .alu_result(alu_result),
     .alu_overflow(alu_overflow),
     .scr1(read_data1),
-    .scr2(read_data2),
+    .scr2(scr2),
     .opcode(opcode),
     .sub_opcode(sub_opcode),
     .enable_execute(enable_execute),
-    .reset(rst));
-//    .reset(reset));
+    .reset(reset));
 
 endmodule
