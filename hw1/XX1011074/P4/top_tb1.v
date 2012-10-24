586899a6e37154acdc519a095f908c5fdb988bcb
Monica Tu <monica.service@gmail.com>
2012-10-24 15:21:52 +0800

    (hw1P4, tb1) Adjust delay at the begging.

--- hw1/XX1011074/P4/top_tb1.v
+++ hw1/XX1011074/P4/top_tb1.v
@@ -4,7 +4,6 @@
 module top_tb1;
 
   parameter DataSize = 32;
-//  parameter AddrSize = 5;
 
   reg [DataSize-1:0]instruction;
   reg clk;
@@ -31,17 +30,18 @@ top TOP (
   #(`PERIOD) reset = 1'b0;
   #(`PERIOD) reset = 1'b1;  
   #(`PERIOD) reset = 1'b0;
+  #(`PERIOD*1.5);
   #(`PERIOD*4) instruction = 32'b0_101000_00000_00000_0000_0000_0001_101; // ADDI
   #(`PERIOD*4) instruction = 32'b0_101000_00001_00001_0000_0000_0001_100; // ADDI
+  #(`PERIOD*4) instruction = 32'b0_100010_00010_0000_0000_0000_0001_0000; // MOVI
   #(`PERIOD*4) instruction = 32'b0_100000_00011_00000_00001_00000_00000; // ADD
   #(`PERIOD*4) instruction = 32'b0_100000_00100_00000_00001_00000_00001; // SUB
   #(`PERIOD*4) instruction = 32'b0_100000_00101_00011_00100_00000_00010; // AND
   #(`PERIOD*4) instruction = 32'b0_100000_00110_00011_00100_00000_00100; // OR
   #(`PERIOD*4) instruction = 32'b0_100000_00111_00011_00100_00000_00011; // XOR
   #(`PERIOD*4) instruction = 32'b0_100000_01000_00000_00100_00000_01000; // SLLI
-  #(`PERIOD*4) instruction = 32'b0_100000_01001_00001_01000_00000_01011; // SRLI
+  #(`PERIOD*4) instruction = 32'b0_100000_01001_00001_01000_00000_01011; // ROTRI
   #(`PERIOD*4) instruction = 32'b0_101011_00001_00001_0000_0000_0010_101; // XORI
-  #(`PERIOD*4) instruction = 32'b0_100010_00010_0000_0000_0000_0001_0000; // MOVI
 
   #(`PERIOD*12) $finish;
   end
