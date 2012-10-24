5d100b1c8c58ba40570f0ee04f60437b49ea405b
Marco.Chen <marco126@gmail.com>
2012-10-24 18:41:00 +0800

    Completed testbench codes for P4 top_tb1.v.(100%)

--- hw1/XX1011074/P4/top_tb1.v
+++ hw1/XX1011074/P4/top_tb1.v
@@ -11,7 +11,16 @@ module top_tb1;
   
   //test &debug
   reg [DataSize-1:0]golden_reg[31:0];
-  reg [31:0]tb_writedata;
+  reg [31:0]tb_rw_reg_0;
+  reg [31:0]tb_rw_reg_1;
+  reg [31:0]tb_rw_reg_2;
+  reg [31:0]tb_rw_reg_3;
+  reg [31:0]tb_rw_reg_4;
+  reg [31:0]tb_rw_reg_5;
+  reg [31:0]tb_rw_reg_6;
+  reg [31:0]tb_rw_reg_7;
+  reg [31:0]tb_rw_reg_8;
+  reg [31:0]tb_rw_reg_9;
   integer i;
   integer err_num; // TODO
 
@@ -41,9 +50,10 @@ top TOP (
   #(`PERIOD*4) instruction = 32'b0_100000_00111_00011_00100_00000_00011; // XOR
   #(`PERIOD*4) instruction = 32'b0_100000_01000_00000_00100_00000_01000; // SLLI
   #(`PERIOD*4) instruction = 32'b0_100000_01001_00001_01000_00000_01011; // ROTRI
+  #(`PERIOD*4) instruction = 32'b0_101100_00000_00000_0000_0000_0011_111; // ORI
   #(`PERIOD*4) instruction = 32'b0_101011_00001_00001_0000_0000_0010_101; // XORI
 
-  #(`PERIOD*12) $finish;
+  #(`PERIOD*4) $finish;
   end
 
   /* Create tb waveform */
@@ -52,52 +62,47 @@ top TOP (
     golden_reg[i] = 32'd0;
   end
 
-  #`PERIOD 
-//  reset = 1'b0;
-//  instruction = 0_101000_00000_00000_0000_0000_0001_101; // ADDI
-  #`PERIOD
-  tb_writedata = 32'b01101;
-  golden_reg[0] = 32'b01101;
+  #(`PERIOD) 
+  #(`PERIOD) 
+  #(`PERIOD) 
+  #(`PERIOD*3.5)
+  #(`PERIOD*4) //ADDI
+  tb_rw_reg_0 = 32'b01101;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //ADDI
+  tb_rw_reg_1 = 32'b01100;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //MOVI
+  tb_rw_reg_2 = 32'b10000;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //ADD
+  tb_rw_reg_3 = 32'b11001;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //SUB
+  tb_rw_reg_4 = 32'b00001;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //AND
+  tb_rw_reg_5 = 32'b00001;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //OR
+  tb_rw_reg_6 = 32'b11001;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //XOR
+  tb_rw_reg_7 = 32'b11000;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //SLLI
+  tb_rw_reg_8 = 32'b11010000;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //ROTRI
+  tb_rw_reg_9 = 32'h0C000000;
 
-  #`PERIOD
-  tb_writedata = 32'b01100;
-  golden_reg[1] = 32'b01100;
+  #(`PERIOD*4) //ORI
+  tb_rw_reg_0 = 32'b11111;
+  
+  #(`PERIOD*4) //XORI
+  tb_rw_reg_1 = 32'b11001;
+
+  #(`PERIOD*4) $finish;
   end
 //  golden_reg[0] = golden_reg[0] + 5'b01101
 //  golden_reg[0] = 5'b01101;
