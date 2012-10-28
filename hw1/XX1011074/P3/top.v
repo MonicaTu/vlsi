89b09597f912656812c36f15bf9b525b4bd885e4
Monica Tu <monica.service@gmail.com>
2012-10-28 22:08:36 +0800

    (hw1P3~P5) Integrate with 3 muxer modules.

--- hw1/XX1011074/P3/top.v
+++ hw1/XX1011074/P3/top.v
@@ -1,5 +1,8 @@
 `include "regfile.v"
 `include "alu32.v"
+`include "mux4to1_select_mux.v"
+`include "imm_reg_select_mux.v"
+`include "writeback_select_mux.v"
 
 // according to spec 
 module top(clk, rst, read_address1, read_address2, write_address, enable_fetch, enable_writeback, imm_5bit, imm_15bit, imm_20bit, mux4to1_select, mux2to1_select, imm_reg_select, enable_execute, opcode, sub_opcode, alu_overflow);
@@ -31,11 +34,11 @@ module top(clk, rst, read_address1, read_address2, write_address, enable_fetch, 
   // others
   wire [DataSize-1:0]read_data1;
   wire [DataSize-1:0]read_data2;
-  wire [DataSize-1:0]scr2;
+  //wire [DataSize-1:0]scr2;
   wire [DataSize-1:0]alu_result;
   wire [DataSize-1:0]write_data;
-  reg  [DataSize-1:0]mux4to1_out;
-  integer i;
+  wire [DataSize-1:0]mux4to1_out;
+  wire [DataSize-1:0]imm_reg_out;
 
   parameter imm5bitZE = 2'b00, imm15bitSE = 2'b01, imm15bitZE = 2'b10, imm20bitSE =  2'b11;
 
@@ -45,7 +48,7 @@ module top(clk, rst, read_address1, read_address2, write_address, enable_fetch, 
     .read_address1(read_address1),
     .read_address2(read_address2),
     .write_address(write_address),
-    .write_data(alu_result),
+    .write_data(write_data),
     .clk(clk),
     .reset(rst),
     .read(enable_fetch),
@@ -55,35 +58,29 @@ module top(clk, rst, read_address1, read_address2, write_address, enable_fetch, 
     .alu_result(alu_result),
     .alu_overflow(alu_overflow),
     .scr1(read_data1),
-    .scr2(scr2),
+    .scr2(imm_reg_out),
     .opcode(opcode),
     .sub_opcode(sub_opcode),
     .enable_execute(enable_execute),
     .reset(rst));
 
-  assign scr2 = (imm_reg_select) ? mux4to1_out: read_data2;
-  assign write_data = (mux2to1_select) ? scr2 : alu_result;
+  mux4to1_select_mux MUX4TO1_SELECT_MUX (
+    .mux4to1_out(mux4to1_out),
+    .imm_5bit(imm_5bit),
+    .imm_15bit(imm_15bit),
+    .imm_20bit(imm_20bit),
+    .mux4to1_select(mux4to1_select));
 
-  always @ (imm_5bit or imm_15bit or imm_20bit or mux4to1_select) begin
-    case (mux4to1_select)
-      imm5bitZE: begin
-        mux4to1_out <= imm_5bit;
-      end
-      imm15bitSE: begin
-        for (i = 31; i > 14; i = i-1)
-          mux4to1_out[i] = imm_15bit[14];
-        mux4to1_out[14:0] = imm_15bit;
-      end
-      imm15bitZE: begin
-        mux4to1_out <= imm_15bit;
-      end
-      imm20bitSE: begin
-        for (i = 31; i > 19; i = i-1)
-          mux4to1_out[i] = imm_20bit[19];
-        mux4to1_out[19:0] = imm_20bit;
-      end
-      default: mux4to1_out <= 32'bx;
-    endcase
-  end
+  imm_reg_select_mux IMM_REG_SELECT_MUX (
+    .imm_reg_out(imm_reg_out),
+    .mux4to1_out(mux4to1_out),
+    .read_data2(read_data2), 
+    .imm_reg_select(imm_reg_select));
+
+  writeback_select_mux WRITEBACK_SELECT_MUX (
+    .write_data(write_data),
+    .imm_reg_out(imm_reg_out),
+    .alu_result(alu_result),
+    .mux2to1_select(mux2to1_select));
 
 endmodule
