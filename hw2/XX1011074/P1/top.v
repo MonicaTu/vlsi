5de99a9efa26bb580c4af3776c8b3584700c0403
Monica Tu <monica.service@gmail.com>
2012-11-03 21:11:26 +0800

    (hw2P1, top) Interface was changed according to p4_top.

--- hw2/XX1011074/P1/top.v
+++ hw2/XX1011074/P1/top.v
@@ -1,15 +1,13 @@
 `include "p4_top.v"
 
-module top (instruction, clk, reset);
+module top (clk, reset);
   
   parameter DataSize = 32;
 
-  input [DataSize-1:0]instruction;
   input clk;
   input reset;
 
   p4_top p4 (
-    .instruction(instruction),
     .clk(clk),
     .reset(reset));
 
