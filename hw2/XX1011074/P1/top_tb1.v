0986df6bc0cc9f342c7072e194123566c6f76105
Monica Tu <monica.service@gmail.com>
2012-11-03 22:07:14 +0800

    (hw2P1, tb) Fix syntax error.

--- hw2/XX1011074/P1/top_tb1.v
+++ hw2/XX1011074/P1/top_tb1.v
@@ -53,7 +53,7 @@ module top_tb1;
 
   /* Create tb waveform */
   initial begin
-  #(`PERIOD*2) 
+  #(`PERIOD*2);
     for ( i = 0; i < DataSize; i = i+1) begin
       golden_reg[i] = 32'd0;
     end
@@ -70,87 +70,87 @@ module top_tb1;
     tb_rw_reg_9 = 32'd0;
     err_num = 0;
 
-  #(`PERIOD*1.5)
-  #(`PERIOD*4)
+  #(`PERIOD*1.5);
+  #(`PERIOD*4);
   #(`PERIOD*4) //ADDI
   tb_rw_reg_0 = 32'b01101;
   golden_reg[0] = 32'b01101;
 
-  #(`PERIOD*4) //ADDI
+  #(`PERIOD*4); //ADDI
   tb_rw_reg_1 = 32'b01100;
   golden_reg[1] = 32'b01100;
   if (tb_rw_reg_0 != TOP.p4.p3.regfile1.rw_reg_0)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //MOVI
+  #(`PERIOD*4); //MOVI
   tb_rw_reg_2 = 32'b10000;
   golden_reg[2] = 32'b10000;
   if (tb_rw_reg_1 != TOP.p4.p3.regfile1.rw_reg_1)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //ADD
+  #(`PERIOD*4); //ADD
   tb_rw_reg_3 = 32'b11001;
   golden_reg[3] = 32'b11001;
   if (tb_rw_reg_2 != TOP.p4.p3.regfile1.rw_reg_2)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //SUB
+  #(`PERIOD*4); //SUB
   tb_rw_reg_4 = 32'b00001;
   golden_reg[4] = 32'b00001;
   if (tb_rw_reg_3 != TOP.p4.p3.regfile1.rw_reg_3)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //AND
+  #(`PERIOD*4); //AND
   tb_rw_reg_5 = 32'b00001;
   golden_reg[5] = 32'b00001;
   if (tb_rw_reg_4 != TOP.p4.p3.regfile1.rw_reg_4)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //OR
+  #(`PERIOD*4); //OR
   tb_rw_reg_6 = 32'b11001;
   golden_reg[6] = 32'b11001;
   if (tb_rw_reg_5 != TOP.p4.p3.regfile1.rw_reg_5)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //XOR
+  #(`PERIOD*4); //XOR
   tb_rw_reg_7 = 32'b11000;
   golden_reg[7] = 32'b11000;
   if (tb_rw_reg_6 != TOP.p4.p3.regfile1.rw_reg_6)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //SLLI
+  #(`PERIOD*4); //SLLI
   tb_rw_reg_8 = 32'b11010000;
   golden_reg[8] = 32'b11010000;
   if (tb_rw_reg_7 != TOP.p4.p3.regfile1.rw_reg_7)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //ROTRI
+  #(`PERIOD*4); //ROTRI
   tb_rw_reg_9 = 32'h0C000000;
   golden_reg[9] = 32'h0C000000;
   if (tb_rw_reg_8 != TOP.p4.p3.regfile1.rw_reg_8)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //ORI
+  #(`PERIOD*4); //ORI
   tb_rw_reg_0 = 32'b11111;
   golden_reg[0] = 32'b11111;
   if (tb_rw_reg_9 != TOP.p4.p3.regfile1.rw_reg_9)
     err_num = err_num + 1;
 
-  #(`PERIOD*4) //XORI
+  #(`PERIOD*4); //XORI
   tb_rw_reg_1 = 32'b11001;
   golden_reg[1] = 32'b11001;
   if (tb_rw_reg_0 != TOP.p4.p3.regfile1.rw_reg_0)
     err_num = err_num + 1;
   
-  #(`PERIOD*4) //IDEL
+  #(`PERIOD*4); //IDEL
   if (tb_rw_reg_1 != TOP.p4.p3.regfile1.rw_reg_1)
     err_num = err_num + 1;
   end
 
   /* Dump and finish */
   initial begin
-  $dumpfile("top_tb1.vcd");
-  $dumpvars;
+    $dumpfile("top_tb1.vcd");
+    $dumpvars;
 //  $fsdbDumpfile("top_tb1.fsdb");
 //  $fsdbDumpvars;
   end
