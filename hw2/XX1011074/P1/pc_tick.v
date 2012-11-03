46da1e2b07e7aa5893e613d910dfa7b594b0257e
Monica Tu <monica.service@gmail.com>
2012-11-03 20:59:48 +0800

    (hw2P1, pc_tick) Add pc = internal_pc/32.

--- hw2/XX1011074/P1/pc_tick.v
+++ hw2/XX1011074/P1/pc_tick.v
@@ -1,23 +1,31 @@
-`define IR_CYCLE 'd4   // 4 clock per ir cycle
-`define TICK_SIZE 128  // 128 bits
-`define IR_SIZE 32     // 32 bits
-`define IR_SIZE_BYTE 4 // 32 bits = 4 bytes
+`define IR_CYCLE 'd4     // 4 clock per ir cycle
+`define TICK_SIZE 'd128  // 128 bits
+`define MEM_SIZE 'd10    // 10 bits
+`define IR_SIZE 'd32     // 32 bits
+`define IR_SIZE_BYTE 'd4 // 32 bits = 4 bytes
 
-module pc_tick(input clock, reset, output reg [`IR_SIZE-1:0] pc, output reg [`TICK_SIZE-1:0] tick);
+module pc_tick(input clock, reset, output reg [`MEM_SIZE-1:0] pc, output reg [`TICK_SIZE-1:0] tick);
+
+  reg [9:0] internal_pc;
 
   always @(negedge clock) begin
     if (reset) begin
+      $display("reset");
+      internal_pc = 0;
       pc = 0;
       tick = 0;
     end
     else begin
       tick = tick + 1;
       if ((tick % `IR_CYCLE) == 0) begin
-        pc = pc + `IR_SIZE_BYTE;
+        internal_pc = internal_pc + `IR_SIZE; 
       end
       else begin
-        pc = pc;
+        internal_pc = internal_pc;
       end
+      $display("pc: %d; internal_pc: %d", pc, internal_pc);
+      pc = internal_pc >> 5; // internal_pc / 32 
+//      $display("pc: %d; internal_pc: %d", pc, internal_pc);
     end
   end
 
