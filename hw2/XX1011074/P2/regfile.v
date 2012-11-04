f30fb69a29c17dbd0354fb0019ef28751ed5e219
Monica Tu <monica.service@gmail.com>
2012-11-05 02:28:13 +0800

    Revert "(hw2P2, regfile) Mark codes for testing."
    This reverts commit bdd57097eedfe1f69677e2c2c8fcfd29396f79ea.

--- hw2/XX1011074/P2/regfile.v
+++ hw2/XX1011074/P2/regfile.v
@@ -17,16 +17,16 @@ module regfile(read_data1, read_data2, read_address1, read_address2,
   integer i;
 
   // for test and debug  
-//  wire [DataSize-1:0]rw_reg_0 = rw_reg[0];
-//  wire [DataSize-1:0]rw_reg_1 = rw_reg[1];
-//  wire [DataSize-1:0]rw_reg_2 = rw_reg[2];
-//  wire [DataSize-1:0]rw_reg_3 = rw_reg[3];
-//  wire [DataSize-1:0]rw_reg_4 = rw_reg[4];
-//  wire [DataSize-1:0]rw_reg_5 = rw_reg[5];
-//  wire [DataSize-1:0]rw_reg_6 = rw_reg[6];
-//  wire [DataSize-1:0]rw_reg_7 = rw_reg[7];
-//  wire [DataSize-1:0]rw_reg_8 = rw_reg[8];
-//  wire [DataSize-1:0]rw_reg_9 = rw_reg[9];
+  wire [DataSize-1:0]rw_reg_0 = rw_reg[0];
+  wire [DataSize-1:0]rw_reg_1 = rw_reg[1];
+  wire [DataSize-1:0]rw_reg_2 = rw_reg[2];
+  wire [DataSize-1:0]rw_reg_3 = rw_reg[3];
+  wire [DataSize-1:0]rw_reg_4 = rw_reg[4];
+  wire [DataSize-1:0]rw_reg_5 = rw_reg[5];
+  wire [DataSize-1:0]rw_reg_6 = rw_reg[6];
+  wire [DataSize-1:0]rw_reg_7 = rw_reg[7];
+  wire [DataSize-1:0]rw_reg_8 = rw_reg[8];
+  wire [DataSize-1:0]rw_reg_9 = rw_reg[9];
 
   always@(posedge clk, posedge reset)begin
     if(reset)begin
