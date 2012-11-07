85188f21b4d14c56bc045dab0dc03909a753203b
Monica Tu <monica.service@gmail.com>
2012-11-07 17:31:48 +0800

    (hw2P4, pc) Connect pc and rom_controller.

--- hw2/XX1011074/P4/pc_tick.v
+++ hw2/XX1011074/P4/pc_tick.v
@@ -1,31 +1,35 @@
-`define IR_CYCLE 'd4     // 4 clock per ir cycle
 `define TICK_SIZE 'd128  // 128 bits
 `define MEM_SIZE 'd10    // 10 bits
 `define IR_SIZE 'd32     // 32 bits
 `define IR_SIZE_BYTE 'd4 // 32 bits = 4 bytes
 
-module pc_tick(input clock, reset, output reg [`MEM_SIZE-1:0] pc, output reg [`TICK_SIZE-1:0] tick);
+module pc_tick(pc, cycle_cnt, cycle, reset, clock);
 
-  reg [9:0] internal_pc;
+  // interface
+  input clock;
+  input reset;
+  input [2:0]cycle;
+  output [`MEM_SIZE-1:0] pc;
+  output [`TICK_SIZE-1:0] cycle_cnt;
+
+  // internal
+  reg [`MEM_SIZE-1:0] pc;
+  reg [`TICK_SIZE-1:0] cycle_cnt;
 
   always @(negedge clock) begin
     if (reset) begin
-      $display("reset");
-      internal_pc = 0;
+//      $display("reset");
       pc = 0;
-      tick = 0;
+      cycle_cnt = 0;
     end
     else begin
-      tick = tick + 1;
-      if ((tick % `IR_CYCLE) == 0) begin
-        internal_pc = internal_pc + `IR_SIZE; 
+      cycle_cnt = cycle_cnt + 1;
+      if ((cycle_cnt % cycle) == 0) begin
+        pc = pc + 1; 
       end
       else begin
-        internal_pc = internal_pc;
+        pc = pc;
       end
-      $display("pc: %d; internal_pc: %d", pc, internal_pc);
-      pc = internal_pc >> 5; // internal_pc / 32 
-//      $display("pc: %d; internal_pc: %d", pc, internal_pc);
     end
   end
 
