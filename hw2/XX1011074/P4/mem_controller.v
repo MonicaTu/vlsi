1ba7ae8d083fe68cd15cefd42b64f215a98eb7b7
Monica Tu <monica.service@gmail.com>
2012-11-08 01:34:16 +0800

    (hw2P2, mem_controller) Use rom_ir instead of instruction.

--- hw2/XX1011074/P4/mem_controller.v
+++ hw2/XX1011074/P4/mem_controller.v
@@ -25,49 +25,49 @@ module mem_controller(im_enable, im_en_read, im_en_write, im_addr, mem_enable, m
   reg mem_en_write; 
   reg [12:0]mem_addr; 
 
-  wire ir_rst    = instruction[35];
-  wire ir_en     = instruction[34];
-  wire ir_select = instruction[33];
-  wire ir_read   = instruction[32];
-  wire ir_start_address = instruction[31:16];
-  wire ir_size   = instruction[15:0];
+  wire ir_rst    = rom_ir[35];
+  wire ir_en     = rom_ir[34];
+  wire ir_select = rom_ir[33];
+  wire ir_read   = rom_ir[32];
+  wire ir_start_address = rom_ir[31:16];
+  wire ir_size   = rom_ir[15:0];
 
   always @ (negedge clock) begin
     if (ir_rst) begin
       if (ir_select == 0) begin // reset IM
-//        im_reset = 1;
-        im_enable = 0;
-        im_en_read = 0;
-        im_en_write = 0;
-        mem_enable = 0;
-        mem_en_read = 0;
-        mem_en_write = 0;
+//        im_reset <= 1;
+        im_enable <= 0;
+        im_en_read <= 0;
+        im_en_write <= 0;
+        mem_enable <= 0;
+        mem_en_read <= 0;
+        mem_en_write <= 0;
       end else begin  // reset DM
-//        im_reset = 0;
-        im_enable = 0;
-        im_en_read = 0;
-        im_en_write = 0;
-        mem_enable = 0;
-        mem_en_read = 0;
-        mem_en_write = 0;
+//        im_reset <= 0;
+        im_enable <= 0;
+        im_en_read <= 0;
+        im_en_write <= 0;
+        mem_enable <= 0;
+        mem_en_read <= 0;
+        mem_en_write <= 0;
       end
     end else begin // load
       if (ir_select == 0) begin // load IM
-//        im_reset = 0;
-        im_enable = 1;
-        im_en_read = 0;
-        im_en_write = 1;
-        mem_enable = 1;
-        mem_en_read = 1;
-        mem_en_write = 0;
+//        im_reset <= 0;
+        im_enable <= 1;
+        im_en_read <= 0;
+        im_en_write <= 1;
+        mem_enable <= 1;
+        mem_en_read <= 1;
+        mem_en_write <= 0;
       end else begin // load DM
-//        im_reset = 0;
-        im_enable = 0;
-        im_en_read = 0;
-        im_en_write = 0;
-        mem_enable = 1;
-        mem_en_read = 1;
-        mem_en_write = 0;
+//        im_reset <= 0;
+        im_enable <= 0;
+        im_en_read <= 0;
+        im_en_write <= 0;
+        mem_enable <= 1;
+        mem_en_read <= 1;
+        mem_en_write <= 0;
       end
     end
   end
