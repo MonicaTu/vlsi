49dc1a16cf0adf8ca84dd61a1b75f663f46374a6
Monica Tu <monica.service@gmail.com>
2012-11-08 01:01:14 +0800

    (hw2P4, MEMORY) Add output messages.

--- hw2/XX1011074/P4/MEMORY.v
+++ hw2/XX1011074/P4/MEMORY.v
@@ -14,6 +14,19 @@ reg [data_size-1:0]mem[mem_size-1:0];
 
 integer i;
 
+// for iverilog testing
+wire [data_size-1:0]mem_0 = mem[0];
+wire [data_size-1:0]mem_1 = mem[1];
+wire [data_size-1:0]mem_2 = mem[2];
+wire [data_size-1:0]mem_3 = mem[3];
+wire [data_size-1:0]mem_4 = mem[4];
+wire [data_size-1:0]mem_5 = mem[5];
+wire [data_size-1:0]mem_6 = mem[6];
+wire [data_size-1:0]mem_7 = mem[7];
+wire [data_size-1:0]mem_8 = mem[8];
+wire [data_size-1:0]mem_9 = mem[9];
+
+
 always@(posedge clk)begin
   if(rst)begin
     for(i=0;i<mem_size;i=i+1)
