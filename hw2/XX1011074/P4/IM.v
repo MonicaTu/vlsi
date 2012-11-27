3fc2c269ca369999a24dcb45fed6934193628a48
Monica Tu <monica.service@gmail.com>
2012-11-27 12:07:58 +0800

    (mater, IM) Set address offset 0x80.

--- hw2/XX1011074/P4/IM.v
+++ hw2/XX1011074/P4/IM.v
@@ -2,6 +2,7 @@ module IM(clk, rst, IM_address, enable_fetch, enable_write, enable_im, IMin, IMo
 
   parameter DataSize=32;
   parameter MemSize=1024;
+  parameter im_start='h80;
   
   input clk, rst, enable_fetch, enable_write, enable_im;
   input [9:0]IM_address;
@@ -16,31 +17,31 @@ module IM(clk, rst, IM_address, enable_fetch, enable_write, enable_im, IMin, IMo
   integer i;
   
   // for test and debug  
-  wire [DataSize-1:0]mem_data_0 = mem_data[0];
-  wire [DataSize-1:0]mem_data_1 = mem_data[1];
-  wire [DataSize-1:0]mem_data_2 = mem_data[2];
-  wire [DataSize-1:0]mem_data_3 = mem_data[3];
-  wire [DataSize-1:0]mem_data_4 = mem_data[4];
-  wire [DataSize-1:0]mem_data_5 = mem_data[5];
-  wire [DataSize-1:0]mem_data_6 = mem_data[6];
-  wire [DataSize-1:0]mem_data_7 = mem_data[7];
-  wire [DataSize-1:0]mem_data_8 = mem_data[8];
-  wire [DataSize-1:0]mem_data_9 = mem_data[9];
-  wire [DataSize-1:0]mem_data_10 = mem_data[10];
-  wire [DataSize-1:0]mem_data_11 = mem_data[11];
-  wire [DataSize-1:0]mem_data_12 = mem_data[12];
-  wire [DataSize-1:0]mem_data_13 = mem_data[13];
-  wire [DataSize-1:0]mem_data_14 = mem_data[14];
-  wire [DataSize-1:0]mem_data_15 = mem_data[15];
-  wire [DataSize-1:0]mem_data_16 = mem_data[16];
-  wire [DataSize-1:0]mem_data_17 = mem_data[17];
-  wire [DataSize-1:0]mem_data_18 = mem_data[18];
-  wire [DataSize-1:0]mem_data_19 = mem_data[19];
-  wire [DataSize-1:0]mem_data_20 = mem_data[20];
-  wire [DataSize-1:0]mem_data_21 = mem_data[21];
-  wire [DataSize-1:0]mem_data_22 = mem_data[22];
-  wire [DataSize-1:0]mem_data_23 = mem_data[23];
-  wire [DataSize-1:0]mem_data_24 = mem_data[24];
+  wire [DataSize-1:0]mem_data_0 = mem_data[im_start+0];
+  wire [DataSize-1:0]mem_data_1 = mem_data[im_start+1];
+  wire [DataSize-1:0]mem_data_2 = mem_data[im_start+2];
+  wire [DataSize-1:0]mem_data_3 = mem_data[im_start+3];
+  wire [DataSize-1:0]mem_data_4 = mem_data[im_start+4];
+  wire [DataSize-1:0]mem_data_5 = mem_data[im_start+5];
+  wire [DataSize-1:0]mem_data_6 = mem_data[im_start+6];
+  wire [DataSize-1:0]mem_data_7 = mem_data[im_start+7];
+  wire [DataSize-1:0]mem_data_8 = mem_data[im_start+8];
+  wire [DataSize-1:0]mem_data_9 = mem_data[im_start+9];
+  wire [DataSize-1:0]mem_data_10 = mem_data[im_start+10];
+  wire [DataSize-1:0]mem_data_11 = mem_data[im_start+11];
+  wire [DataSize-1:0]mem_data_12 = mem_data[im_start+12];
+  wire [DataSize-1:0]mem_data_13 = mem_data[im_start+13];
+  wire [DataSize-1:0]mem_data_14 = mem_data[im_start+14];
+  wire [DataSize-1:0]mem_data_15 = mem_data[im_start+15];
+  wire [DataSize-1:0]mem_data_16 = mem_data[im_start+16];
+  wire [DataSize-1:0]mem_data_17 = mem_data[im_start+17];
+  wire [DataSize-1:0]mem_data_18 = mem_data[im_start+18];
+  wire [DataSize-1:0]mem_data_19 = mem_data[im_start+19];
+  wire [DataSize-1:0]mem_data_20 = mem_data[im_start+20];
+  wire [DataSize-1:0]mem_data_21 = mem_data[im_start+21];
+  wire [DataSize-1:0]mem_data_22 = mem_data[im_start+22];
+  wire [DataSize-1:0]mem_data_23 = mem_data[im_start+23];
+  wire [DataSize-1:0]mem_data_24 = mem_data[im_start+24];
 
   always@(posedge clk)
   begin
