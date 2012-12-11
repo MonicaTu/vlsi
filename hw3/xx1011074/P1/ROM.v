a43269255a006e6ce8c1f96e7116f083ede24db0
Monica Tu <monica.service@gmail.com>
2012-12-11 17:28:27 +0800

    (hw3P1, ROM) Modify port name according to hw3 table2.

--- hw3/xx1011074/P1/ROM.v
+++ hw3/xx1011074/P1/ROM.v
@@ -1,14 +1,17 @@
-module rom(clk, read, enable, address, dout);
+module rom(clk, rom_enable, rom_address, rom_out);
 
 parameter data_size=36;
-parameter mem_size=256;
+parameter address_size=8;
+parameter mem_size=(2**address_size);
 
-input clk, read, enable;
-input [7:0]address;
+input clk, rom_enable;
+input [address_size-1:0]rom_address;
 
-output [data_size-1:0]dout;
+output [data_size-1:0]rom_out;
 
-reg [data_size-1:0]dout;
+reg [data_size-1:0]rom_out;
+
+// internal
 reg [data_size-1:0]mem_data[mem_size-1:0];
 
 // for test
@@ -20,11 +23,9 @@ wire [data_size-1:0]rom_mem_data_4 = mem_data[4];
 
 
 always@(posedge clk)begin
-  if(enable)begin
-    if(read)begin
-      dout<=mem_data[address];
+  if(rom_enable)begin
+      rom_out<=mem_data[rom_address];
       #1; // FIXME: workaround for time delay
-    end
   end
 end
 
