ae715e92ddf738edfd74e3399711b841abc8122e
Monica Tu <monica.service@gmail.com>
2012-12-14 15:04:01 +0800

    (hw3P1, sign_extend) Fix syntax error.

--- hw3/xx1011074/P1/sign_extend_20to32.v
+++ hw3/xx1011074/P1/sign_extend_20to32.v
@@ -3,8 +3,8 @@ module sign_extend_20to32(o_data, i_data);
   input	 [19:0] i_data;
   
   output [31:0]o_data;
-  reg	   [31:0]o_data;
+//  reg	   [31:0]o_data;
   
-  assign o_data = {(31-20){{i_data[20]}}, i_data};
+  assign o_data = {{(31-20){i_data[20]}}, i_data};
 	
 endmodule
--- hw3/xx1011074/P1/sign_extend_5to32.v
+++ hw3/xx1011074/P1/sign_extend_5to32.v
@@ -3,8 +3,8 @@ module sign_extend_5to32(o_data, i_data);
   input	 [4:0] i_data;
   
   output [31:0]o_data;
-  reg	   [31:0]o_data;
+//  reg	   [31:0]o_data;
   
-  assign o_data = {(31-5){{i_data[5]}}, i_data};
+  assign o_data = {{(31-5){i_data[5]}}, i_data};
 	
 endmodule
