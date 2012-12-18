2c677ecd0f16599e2a74cef0afc6f4081e4236e0
Monica Tu <monica.service@gmail.com>
2012-12-18 22:45:08 +0800

    (hw3) Fix bit number of sign_extend module.

--- hw3/xx1011074/P1/sign_extend_20to32.v
+++ hw3/xx1011074/P1/sign_extend_20to32.v
@@ -5,6 +5,6 @@ module sign_extend_20to32(o_data, i_data);
   output [31:0]o_data;
 //  reg	   [31:0]o_data;
   
-  assign o_data = {{(31-20){i_data[20]}}, i_data};
+  assign o_data = {{(31-19){i_data[19]}}, i_data};
 	
 endmodule
--- hw3/xx1011074/P1/sign_extend_24to32.v
+++ hw3/xx1011074/P1/sign_extend_24to32.v
@@ -5,6 +5,6 @@ module sign_extend_24to32(o_data, i_data);
   output [31:0]o_data;
 //  reg	   [31:0]o_data;
   
-  assign o_data = {{(31-24){i_data[24]}}, i_data};
+  assign o_data = {{(31-23){i_data[23]}}, i_data};
 	
 endmodule
