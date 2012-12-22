4e9e3a787f701915230350461e7a8809a3156b76
Monica Tu <monica.service@gmail.com>
2012-12-22 18:48:45 +0800

    (hw3, zero_extend_15to32) Fix error: Concatenation operand "'sd0" has indefinite width.

--- hw3/xx1011074/P1/zero_extend_15to32.v
+++ hw3/xx1011074/P1/zero_extend_15to32.v
@@ -5,6 +5,7 @@ module zero_extend_15to32(o_data, i_data);
   output [31:0]o_data;
 //  reg	   [31:0]o_data;
   
-  assign o_data = {{(31-14){0}}, i_data};
+//  assign o_data = {{(31-14){0}}, i_data};
+  assign o_data = i_data;
 	
 endmodule
