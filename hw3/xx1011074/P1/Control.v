a5744cc1543c479bb5c660560b6a7b373e0e4fe7
Monica Tu <monica.service@gmail.com>
2012-12-22 18:15:55 +0800

    (hw3) Fix syntax error in Control.

--- hw3/xx1011074/P1/Control.v
+++ hw3/xx1011074/P1/Control.v
@@ -1,8 +1,11 @@
-module Control(opcode, sub_opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
+module Control(ImmSelect, ALUOp, ALUSubOp, RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, opcode, sub_opcode);
    
   input [5:0]opcode;
   input [4:0]sub_opcode;
 
+  output [3:0]ImmSelect;
+  output [5:0]ALUOp;
+  output [4:0]ALUSubOp;
   output RegDst;    // Rb = scr2
   output Branch;    //
   output MemRead;   // read Mem 
@@ -13,7 +16,10 @@ module Control(opcode, sub_opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, Mem
 
   // internal
   reg [6:0]out;
+  reg [3:0]ImmSelect;
   
+  assign ALUOp = opcode;
+  assign ALUSubOp = sub_opcode;
   assign RegDst = out[6];
   assign Branch = out[5];
   assign MemRead = out[4];
@@ -26,36 +32,67 @@ module Control(opcode, sub_opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, Mem
   always @ (opcode) begin
     case (opcode)
       6'b100000:    // ADD/SUB/AND/OR/XOR
-        out = 7'b1000001;
         case (sub_opcode)
-          5'b01001: // SRLI, FIXME: NOP
-          5'b01000: // SLLI
-          5'b01011: // ROTRI
+          5'b01001: begin// SRLI, FIXME: NOP
+            ImmSelect = 3'b001; // imm5
             out = 7'b0000001;
+          end
+          5'b01000: begin// SLLI
+            ImmSelect = 3'b001; // imm5
+            out = 7'b0000001;
+          end
+          5'b01011: begin// ROTRI
+            ImmSelect = 3'b001; // imm5
+            out = 7'b0000001;
+          end
+          default: begin
+            ImmSelect = 3'b000; // imm0
+            out = 7'b1000001;
+          end
         endcase
-      6'b101000:    // ADDI
+      6'b101000: begin   // ADDI
+        ImmSelect = 3'b010; // imm15
         out = 7'b0000001;
-      6'b101100:    // ORI
+        end
+      6'b101100: begin   // ORI
+        ImmSelect = 3'b010; // imm15
         out = 7'b0000001;
-      6'b101011:    // XORI
+        end
+      6'b101011: begin   // XORI
+        ImmSelect = 3'b010; // imm15
         out = 7'b0000001;
-      6'b000010:    // LWI
+        end
+      6'b000010: begin   // LWI
+        ImmSelect = 3'b010; // imm15
         out = 7'b0011011;
-      6'b001010:    // SWI
+        end
+      6'b001010: begin   // SWI
+        ImmSelect = 3'b010; // imm15
         out = 7'b0000110;
-      6'b100010:    // MOVI
+        end
+      6'b100010: begin   // MOVI
+        ImmSelect = 3'b011; // imm20
         out = 7'b0000001;
+        end
       6'b011100:
         case (sub_opcode)
-          5'b00010: // LW
+          5'b00010: begin // LW
+            ImmSelect = 3'b000; // imm0
             out = 7'b0011011;
-          5'b01010: // SW
+            end
+          5'b01010: begin // SW
+            ImmSelect = 3'b000; // imm0
             out = 7'b0000110;
+            end
         endcase
-      6'b100110:    // BEQ
+      6'b100110: begin   // BEQ
+        ImmSelect = 3'b100; // imm14
         out = 7'b0100000;
-      6'b100100:    // J
+        end
+      6'b100100: begin   // J
+        ImmSelect = 3'b101; // imm24
         out = 7'b0100000;
+        end
     endcase
   end
 endmodule
