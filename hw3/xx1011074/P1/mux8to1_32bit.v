d8696959a957cb847f177b8726ce2e0e312ecf12
Monica Tu <monica.service@gmail.com>
2012-12-22 18:11:23 +0800

    (hw3) Fix syntax error in mux8to1_32bit.

--- hw3/xx1011074/P1/mux8to1_32bit.v
+++ hw3/xx1011074/P1/mux8to1_32bit.v
@@ -13,15 +13,16 @@ module mux8to1_32bit (o_data, i_data0, i_data1, i_data2, i_data3, i_data4, i_dat
   output [31:0]o_data;
   reg    [31:0]o_data;
 
-  case (i_select) begin
-    3'b000  : o_data = i_data0;
-    3'b001  : o_data = i_data1;
-    3'b010  : o_data = i_data2;
-    3'b011  : o_data = i_data3;
-    3'b100  : o_data = i_data4;
-    3'b101  : o_data = i_data5;
-    3'b110  : o_data = i_data6;
-    3'b111  : o_data = i_data7;
-  endcase
-
+  always @ (*) begin
+    case (i_select)
+      3'b000: o_data = i_data0;
+      3'b001: o_data = i_data1;
+      3'b010: o_data = i_data2;
+      3'b011: o_data = i_data3;
+      3'b100: o_data = i_data4;
+      3'b101: o_data = i_data5;
+      3'b110: o_data = i_data6;
+      3'b111: o_data = i_data7;
+    endcase
+  end
 endmodule
